39cf71ce1b9ff4c6ec86174e9b696db2c95bd510
RafaelReis891 <geraldorafaelreis@gmail.com>
2021-02-18 15:29:24 -0300

    Smooth Follow 2D

---
+++ SmoothFollow2D.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow2D : MonoBehaviour {
+
+    public float dampTime = 0.3f; //offset from the viewport center to fix damping
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+    public float m_XOffset, m_YOffset;
+
+
+ void Update()
+    {
+        if (target)
+        {
+            Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta + new Vector3(m_XOffset, m_YOffset, 0);
+
+            // Set this to the Y position you want the camera locked to
+             destination.y = m_YOffset;
+
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }
+    }
+}
